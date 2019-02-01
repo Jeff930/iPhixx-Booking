1032707135b5bf00d2926cffabc2b56ff89f96b7
Unknown <jefferson30.jdg@gmail.com>
2019-02-01 15:56:09 +0800

    Added id for phone brands model file

--- src/models/phonebrands.ts
+++ src/models/phonebrands.ts
@@ -1,9 +1,15 @@
 export const phonebrands = [
-	{ brand: 'iPhone' },
-	{ brand: 'Samsung' },
-	{ brand: 'Huawei' },
-	{ brand: 'Sony' },
-	{ brand: 'Nokia' },
-	{ brand: 'Other' },
+	{ 	key:'1',
+		brand: 'iPhone' },
+	{ 	key:'2',
+		brand: 'Samsung' },
+	{ 	key:'3',
+		brand: 'Huawei' },
+	{	key:'4', 
+		brand: 'Sony' },
+	{ 	key:'5',
+		brand: 'Nokia' },
+	{	key:null, 
+		brand: 'Other' },
 	// { brand: 'Other' }
 ]
