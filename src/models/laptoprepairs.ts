6a11a9868fef19b87d9afc6f903455e2309fa451
Unknown <jefferson30.jdg@gmail.com>
2019-02-05 23:00:10 +0800

    Added laptop repairs

---
+++ src/models/laptoprepairs.ts
@@ -0,0 +1,12 @@
+export const laptoprepairs = [
+	{ repair : 'Screen Replacement'},
+	{ repair : 'Keyboard Replacement'},
+	{ repair : 'Fan Replacement'},
+	{ repair : 'Camera Repair'},
+	{ repair : 'Speaker Repair'},
+	{ repair : 'Hard Drive Replacement'},
+	{ repair : 'Charging Port Repair'},
+	{ repair : 'Data Recovery'},
+	{ repair : 'Battery Replacement'},
+	{ repair : 'Virus Removal'},
+];
