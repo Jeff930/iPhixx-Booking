be13ebbe104fffcd193974aafcf1d430628d4d82
Unknown <jefferson30.jdg@gmail.com>
2019-02-15 21:56:38 +0800

    Removed unused import

--- src/pages/bookingp/bookingp.ts
+++ src/pages/bookingp/bookingp.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 
 // import { Http, Headers } from '@angular/http';
 // import { Observable } from 'rxjs/Observable';
