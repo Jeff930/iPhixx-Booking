c1515bc0a242bd9c0f4c614f9a3ec1f877ba484f
Unknown <jefferson30.jdg@gmail.com>
2019-02-15 21:58:59 +0800

    Code cleanup

--- src/pages/passcode/passcode.ts
+++ src/pages/passcode/passcode.ts
@@ -1,4 +1,4 @@
-import * as $ from "jquery";
+//import * as $ from "jquery";
 import { Component } from '@angular/core';
 import { NavigationProvider } from '../../providers/navigation/navigation';
 import {NavController, NavParams , LoadingController} from 'ionic-angular';
