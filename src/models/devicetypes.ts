b3a5f98c658bf0eb553a7c270fec332851831687
Unknown <jefferson30.jdg@gmail.com>
2019-03-19 00:24:07 +0800

    Added keys for device type

--- src/models/devicetypes.ts
+++ src/models/devicetypes.ts
@@ -1,21 +1,22 @@
 export const devices = [
 	{	
+		key:'1',
 		title: 'Phone',
 		img: 'assets/imgs/phone.png' 
 	},
-	{	
+	{	key:'2',
 		title: 'Tablet',
 		img: 'assets/imgs/tablet.png' 
 	},
-	{	
+	{	key:'3',
 		title: 'Laptop',
 		img: 'assets/imgs/tablet.png' 
 	},
-	{	
+	{	key:'4',
 		title: 'MacBook',
 		img: 'assets/imgs/tablet.png' 
 	},
-	{	
+	{	key:'5',
 		title: 'Gaming Console',
 		img: 'assets/imgs/tablet.png' 
 	},
