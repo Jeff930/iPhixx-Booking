50c85fdd3857d0eb2e57deeff7e69ef45a291ea4
Unknown <jefferson30.jdg@gmail.com>
2019-05-09 16:20:55 +0800

    Added search functionality

--- src/pages/customerdetails/customerdetails.ts
+++ src/pages/customerdetails/customerdetails.ts
@@ -12,7 +12,6 @@ import { CartProvider } from '../../providers/cart/cart';
 import { ConfirmationPage } from '../../pages/confirmation/confirmation';
 
 
-
 /**
  * Generated class for the CustomerdetailsPage page.
  *
@@ -101,6 +100,9 @@ export class CustomerdetailsPage {
   customRepair="";
   customRepairPrice;
 
+  searchInput="";
+  filter=false;
+
   locations;
   selectedRepairs = this.cart.selectedRepairs;
   action="Create Customer";
@@ -157,7 +159,55 @@ export class CustomerdetailsPage {
 		this.navigation.activePageIndex=15;
   }
 
-  ionViewWillLoad() {
+  Search(){
+	this.filter=true;
+	let loading = this.loadingCtrl.create({
+		//content: 'Creating Customer...'
+	 });
+	 loading.present();
+	 let xhr = new XMLHttpRequest();
+	 //xhr.withCredentials = true;
+ 
+	 xhr.addEventListener("readystatechange",  () =>{
+	   if (xhr.readyState === 4) {
+		 console.log(xhr.responseText);
+		 let result = JSON.parse(xhr.responseText);
+		 this.customers =result.customers;
+		 console.log(this.customers);
+		 //console.log("id",result.customer.id);
+		   if(this.customers!=undefined){
+			 loading.dismiss();
+			 //localStorage.setItem('authenticated' , JSON.stringify(result));
+			 //this.booking.userData.customer_id = result.user_id;
+			 //console.log(result.customer.id);
+			 //this.createTicket(user,result.customer.id);
+			 //this.navCtrl.setRoot(ConfirmationPage);
+		  }
+		   else{
+			loading.dismiss();
+			 let alert = this.alertCtrl.create({
+			   title: 'Error: Customers Details Not Received',
+			   subTitle: this.customers.message,
+			   buttons: ['Ok']
+			 });
+			 alert.present();
+		  }
+	   }
+	 });
+	 console.log(JSON.stringify(this.booking.userData));
+	 var userData = JSON.stringify(this.booking.userData);
+	 userData = userData.substring(1,userData.length-1);
+	 console.log(userData);
+	 // var url = "https://cors-anywhere.herokuapp.com/https://iphixx.repairshopr.com/api/v1/customers?api_key=79bc78aa-81d3-4d8c-94db-5a07a0374670&email="+
+	 // 	user.email+"&mobile="+user.phone+"&lastname="+user.lastname+"&firstname="+user.firstname+"&phone="+user.phone+"&properties="+JSON.stringify(properties);
+		 //+"&properties="+userData;
+		 var url = "https://cors-anywhere.herokuapp.com/https://iphixx.repairshopr.com/api/v1/customers?api_key=8e5044d0-6f23-49ef-9c9a-25c516f3debc&query="+this.searchInput;
+	 console.log(url);
+	 xhr.open("GET", url);
+	// xhr.open("POST", "https://admin.iphixx.com/api/v1/customers/sign-in");
+ 
+	 xhr.send();
+
   }
 
   selectCustomer(){
@@ -166,6 +216,7 @@ export class CustomerdetailsPage {
   }
 
   getCustomerList(page){
+	this.filter=false;
 	let loading = this.loadingCtrl.create({
 		//content: 'Creating Customer...'
 	 });
