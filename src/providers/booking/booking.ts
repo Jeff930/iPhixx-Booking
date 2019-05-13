402a522342fc58a6fb29c1ab7f38390883046c40
Unknown <jefferson30.jdg@gmail.com>
2019-05-13 13:25:04 +0800

    Added location

--- src/providers/booking/booking.ts
+++ src/providers/booking/booking.ts
@@ -73,7 +73,8 @@ export class BookingProvider {
           email: '',
           phone: '',
           phone2: '',
-          pin : ''  
+          pin : '',
+          location:'',  
           }
     }
   ];
