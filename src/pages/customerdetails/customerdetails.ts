5256b5809f3ed4e865f48f120354fef9d64fbb51
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2019-08-20 15:25:18 +0800

    Re-enabled validator in customer creation

--- src/pages/customerdetails/customerdetails.ts
+++ src/pages/customerdetails/customerdetails.ts
@@ -462,7 +462,7 @@ export class CustomerdetailsPage {
 	// 	//content: 'Creating Customer...'
 	//  });
 	//  loading.present();
-	//  loading.dismiss();
+
 		let xhr = new XMLHttpRequest();
 		//xhr.withCredentials = true;
 	
@@ -470,31 +470,29 @@ export class CustomerdetailsPage {
 			
 		  if (xhr.readyState === 4) {
 			console.log(xhr.responseText);
-			
-			// let result = JSON.parse(xhr.responseText);
-			// console.log(result);
-			//console.log("id",result.customer.id);
-			//   if(result.customer!=undefined){
-			// 	loading.dismiss();
-			// 	//localStorage.setItem('authenticated' , JSON.stringify(result));
-			// 	//this.booking.userData.customer_id = result.user_id;
-			// 	console.log(result.customer.id);
-			// 	//this.createTicket(user,result.customer.id);
-			// 	//this.navCtrl.setRoot(ConfirmationPage);
-			//  }else{
-			// 	loading.dismiss();
-			// 	let message = result.message;
-			// 	if (result.message=="Email has already been taken")
-			// 		message = "Customer Creation failed due to Duplicate Email";
-			// 	console.log(result.message);
-			// 	console.log(message);
-			// 	let alert = this.alertCtrl.create({
-			// 	  title: 'Error: Customer Not Created',
-			// 	  subTitle: message,
-			// 	  buttons: ['Ok']
-			// 	});
-			// 	alert.present();
-			//  }
+			let result = JSON.parse(xhr.responseText);
+			console.log(result);
+			console.log(result.id);
+			  if(result.id!=undefined){
+				//loading.dismiss();
+				//localStorage.setItem('authenticated' , JSON.stringify(result));
+				//this.booking.userData.customer_id = result.user_id;
+				console.log(result.customer.id);
+				//this.createTicket(user,result.customer.id);
+				//this.navCtrl.setRoot(ConfirmationPage);
+			 }else{
+				//loading.dismiss();
+				let message = result.message;
+				if (result.message=="Email has already been taken")
+					message = "Customer Creation failed due to Duplicate Email";
+				console.log(result);
+				let alert = this.alertCtrl.create({
+				  title: 'Error: Customer Not Created',
+				  subTitle: message,
+				  buttons: ['Ok']
+				});
+				alert.present();
+			 }
 		  }
 		});
 		console.log(JSON.stringify(this.booking.userData));
